39e20f0ae8505f056ada7e8d3326f8c91aaaedcb
Whelan Workmaster <christianworkmaster@gmail.com>
2016-03-04 23:35:54 -0500

    Started the GUI for Reservation/ Customer
    attempted to make a gui by hand

---
+++ Hotel_Management_System/src/testView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import ResCust.*;
+/**
+ *
+ * @author WhelanMyPC
+ */
+public class testView {
+    
+    public static void main(String[] args) {
+        
+        ReservationView r = new ReservationView();
+    }
+    
+}
